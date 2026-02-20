c028a1f23fb267a397d43a80c5b413ccb0321db1
Mahmoud-Sa1ah <mahmoudsalah12341992@gmail.com>
2026-02-20 04:55:07 +0200

    fix: remove basePath for user site

--- next.config.ts
+++ next.config.ts
@@ -6,15 +6,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-
-  basePath: '/m0xsa1ah',
-  assetPrefix: '/m0xsa1ah/',
-
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
-const withMDX = createMDX({
-  // optional remark/rehype plugins
-})
+const withMDX = createMDX({})
 
 export default withMDX(nextConfig)
